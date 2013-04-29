951f54fb5e6833a5047cf7750127d88b686cbd41
Mat Ryer <matryer@mac.com>
2013-04-29 17:19:48 -0600

    tweaked the way the HttpHandler manages handlers

--- handlers/http_handler.go
+++ handlers/http_handler.go
@@ -1,22 +1,18 @@
 package handlers
 
 import (
-	//"github.com/stretchrcom/goweb/context"
+	"github.com/stretchrcom/goweb/context"
 	"net/http"
 )
 
 type HttpHandler struct {
-	handlers Pipe
+	Handlers Pipe
 }
 
 func NewHttpHandler() *HttpHandler {
-	h := new(HttpHandler)
-	h.handlers = make(Pipe, 3)
 
-	// make the default pipes
-	h.handlers[0] = make(Pipe, 0)
-	h.handlers[1] = make(Pipe, 0)
-	h.handlers[2] = make(Pipe, 0)
+	h := new(HttpHandler)
+	h.Handlers = make(Pipe, 0)
 
 	return h
 }
@@ -24,24 +20,15 @@ func NewHttpHandler() *HttpHandler {
 func (handler *HttpHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
 
 	// make the context
-	//ctx := context.NewContext(request.URL.Path)
+	ctx := context.NewContext(request.URL.Path)
 
 	// run it through the handlers
+	err := handler.Handlers.Handle(ctx)
 
-}
+	if err != nil {
 
-/*
-  Handlers
-*/
+		// TODO: handle errors
 
-func (h *HttpHandler) Handlers() Pipe {
-	return h.handlers[1].(Pipe)
-}
+	}
 
-func (h *HttpHandler) BeforeHandlers() Pipe {
-	return h.handlers[0].(Pipe)
-}
-
-func (h *HttpHandler) AfterHandlers() Pipe {
-	return h.handlers[2].(Pipe)
 }
--- handlers/http_handler_test.go
+++ handlers/http_handler_test.go
@@ -1,34 +1,35 @@
 package handlers
 
 import (
-	"github.com/stretchrcom/testify/assert"
+	handlers_test "github.com/stretchrcom/goweb/handlers/test"
+	"github.com/stretchrcom/testify/mock"
+	"net/http"
 	"testing"
 )
 
-func TestNewHttpHandler(t *testing.T) {
+func TestServeHTTP(t *testing.T) {
 
-	h := NewHttpHandler()
+	testRequest, _ := http.NewRequest("GET", "http://github.com/strecthrcom/goweb", nil)
+	handler := NewHttpHandler()
 
-	if assert.NotNil(t, h) {
-		if assert.NotNil(t, h.handlers) {
-			if assert.Equal(t, 3, len(h.handlers)) {
+	// setup some test handlers
+	handler1 := new(handlers_test.TestHandler)
+	handler2 := new(handlers_test.TestHandler)
+	handler3 := new(handlers_test.TestHandler)
 
-				assert.NotNil(t, h.handlers[0])
-				assert.NotNil(t, h.handlers[1])
-				assert.NotNil(t, h.handlers[2])
+	handler.Handlers = append(handler.Handlers, handler1)
+	handler.Handlers = append(handler.Handlers, handler2)
+	handler.Handlers = append(handler.Handlers, handler3)
 
-			}
-		}
-	}
+	handler1.On("WillHandle", mock.Anything).Return(true, nil)
+	handler1.On("Handle", mock.Anything).Return(nil)
+	handler2.On("WillHandle", mock.Anything).Return(true, nil)
+	handler2.On("Handle", mock.Anything).Return(nil)
+	handler3.On("WillHandle", mock.Anything).Return(true, nil)
+	handler3.On("Handle", mock.Anything).Return(nil)
 
-}
+	handler.ServeHTTP(nil, testRequest)
 
-func TestHandlers(t *testing.T) {
-
-	h := NewHttpHandler()
-
-	assert.Equal(t, h.handlers[0], h.BeforeHandlers())
-	assert.Equal(t, h.handlers[1], h.Handlers())
-	assert.Equal(t, h.handlers[2], h.AfterHandlers())
+	mock.AssertExpectationsForObjects(t, handler1.Mock, handler2.Mock, handler3.Mock)
 
 }
