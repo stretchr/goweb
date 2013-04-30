f73889333b8f7ebfea642bd2efdbc3ee3815ebde
Mat Ryer <matryer@mac.com>
2013-04-29 21:40:48 -0600

    started adding test webapp

---
+++ example_webapp/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/stretchrcom/goweb"
+	"github.com/stretchrcom/goweb/context"
+)
+
+func main() {
+
+	goweb.Map("people/{id}", func(c *context.Context) {
+
+	})
+
+}
