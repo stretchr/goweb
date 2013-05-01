7a2d3c474d6bf7645e0b8c8e258fe1e07aef1f56
Mat Ryer <matryer@mac.com>
2013-05-01 17:48:21 -0600

    added comments to path match

--- paths/path_match.go
+++ paths/path_match.go
@@ -4,9 +4,19 @@ import (
 	"github.com/stretchrcom/stew/objects"
 )
 
+/*
+  PathMatch holds details about whether a path matches a PathPattern or not.
+
+  If it does match, the Parameters map will contain the values for any
+  dynamic parameters discovered.
+*/
 type PathMatch struct {
 	Matches    bool
 	Parameters objects.Map
 }
 
+/*
+  PathDoesntMatch is a special instance of PathMatch that indicates the paths
+  do not match.
+*/
 var PathDoesntMatch *PathMatch = new(PathMatch)
