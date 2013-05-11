3508edc4d6d7c6bbc6c4d6fa7c2513b778ab3782
Mat Ryer <matryer@mac.com>
2013-05-11 09:34:01 -0600

    added path prefix getter

---
+++ paths/path_prefix_test.go
@@ -0,0 +1,19 @@
+package paths
+
+import (
+  "github.com/stretchrcom/testify/assert"
+  "testing"
+)
+
+type ChildController struct{}
+type ChildWithMultipleWordsController struct{}
+
+func TestPathPrefixForClass(t *testing.T) {
+
+  c := new(ChildController)
+  assert.Equal(t, PathPrefixForClass(c), "child")
+
+  c2 := new(ChildWithMultipleWordsController)
+  assert.Equal(t, PathPrefixForClass(c2), "child-with-multiple-words")
+
+}
