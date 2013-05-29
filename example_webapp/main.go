0514b226c56f5f6781ff32c6472238e355dd3992
Mat Ryer <matryer@mac.com>
2013-05-28 21:34:34 -0600

    added restful controller example

--- example_webapp/main.go
+++ example_webapp/main.go
@@ -65,6 +65,12 @@ func main() {
 	})
 
 	/*
+		RESTful controller
+	*/
+	thingsController := new(ThingsController)
+	goweb.MapController(thingsController)
+
+	/*
 		Catch-all handler for everything that we don't understand
 	*/
 	goweb.Map(func(c context.Context) error {
@@ -110,6 +116,9 @@ func main() {
 	log.Printf("\t  http://localhost%s/people/me (will redirect)", Address)
 	log.Printf("\t  http://localhost%s/errortest", Address)
 
+	log.Println("")
+	log.Printf("%s", goweb.DefaultHttpHandler())
+
 	go func() {
 		for _ = range c {
 
@@ -139,5 +148,86 @@ func main() {
 	   END OF WEB SERVER CODE
 
 	*/
+
+}
+
+/*
+	RESTful example
+*/
+
+// Thing is just a thing
+type Thing struct {
+	Id   string
+	Text string
+}
+
+// ThingsController is the RESTful MVC controller for Things.
+type ThingsController struct {
+	// Things holds the things... obviously, you would never do this
+	// in the real world - you'd be reading from some kind of datastore.
+	Things []*Thing
+}
+
+func (r *ThingsController) Create(ctx context.Context) error {
+
+	data, dataErr := ctx.RequestData()
+
+	if dataErr != nil {
+		return goweb.API.RespondWithError(ctx, http.StatusInternalServerError, dataErr.Error())
+	}
+
+	dataMap := data.(map[string]interface{})
+
+	thing := new(Thing)
+	thing.Id = dataMap["Id"].(string)
+	thing.Text = dataMap["Text"].(string)
+
+	r.Things = append(r.Things, thing)
+
+	return goweb.Respond.WithStatus(ctx, http.StatusCreated)
+
+}
+
+func (r *ThingsController) ReadMany(ctx context.Context) error {
+
+	if r.Things == nil {
+		r.Things = make([]*Thing, 0)
+	}
+
+	return goweb.API.RespondWithData(ctx, r.Things)
+}
+
+func (r *ThingsController) Read(id string, ctx context.Context) error {
+
+	for _, thing := range r.Things {
+		if thing.Id == id {
+			return goweb.API.RespondWithData(ctx, thing)
+		}
+	}
+
+	return goweb.Respond.WithStatus(ctx, http.StatusNotFound)
+
+}
+
+func (r *ThingsController) DeleteMany(ctx context.Context) error {
+
+	r.Things = make([]*Thing, 0)
+	return goweb.Respond.WithOK(ctx)
+
+}
+
+func (r *ThingsController) Delete(id string, ctx context.Context) error {
+
+	newThings := make([]*Thing, 0)
+
+	for _, thing := range r.Things {
+		if thing.Id != id {
+			newThings = append(newThings, thing)
+		}
+	}
+
+	r.Things = newThings
+
+	return goweb.Respond.WithOK(ctx)
 
 }
