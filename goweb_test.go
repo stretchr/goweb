c94e3238bfa56ca5ec4028b9f1d5c47893729f8f
Mat Ryer <matryer@mac.com>
2013-06-04 11:08:24 -0600

    added comment

--- goweb_test.go
+++ goweb_test.go
@@ -4,4 +4,6 @@ import (
 	"testing"
 )
 
+// TestSomething doesn't actually test anything (weird eh?) but it causes our
+// test packages to actually build (and therefore validate) this package.
 func TestSomething(t *testing.T) {}
