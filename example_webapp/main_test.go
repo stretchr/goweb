544d3de9d8e6473c2e5d4b9787159970af2685c0
Mat Ryer <matryer@mac.com>
2013-06-11 17:01:08 -0600

    started writing test code

--- example_webapp/main_test.go
+++ example_webapp/main_test.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"github.com/stretchrcom/goweb/handlers"
+	//"github.com/stretchrcom/testify/assert"
+	"testing"
+)
+
+func TestRoutes(t *testing.T) {
+
+	// make a test HttpHandler and use it
+	handler := new(handlers.HttpHandler)
+	SetDefaultHttpHandler(handler)
+
+	// call the target code
+	mapRoutes()
+
+	// TODO: make assertions
+
+	/*
+
+	   we want to be able to do things like this:
+
+	     handlers := handler.GetHandlersFor("GET", "people/123")
+	     h := handler.HandlerForRequest(request)
+
+	*/
+
+}
