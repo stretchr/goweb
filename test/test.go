27396204061b27109db9b3a020352ae69fecc00a
Tyler Bunnell <tylerbunnell@gmail.com>
2013-06-12 09:34:49 -0600

    Fix missing type

--- test/test.go
+++ test/test.go
@@ -1,12 +1,15 @@
 package test
 
+import (
+	"net/http"
+)
+
 func TestWeb(t *testing.T) {
 
-  goweb.Test(t, "GET people/123", func(t *testing.T, response) {
+	goweb.Test(t, "GET people/123", func(t *testing.T, response http.Response) {
 
-    assert.Equal(t, 200, response.StatusCode())
+		assert.Equal(t, 200, response.StatusCode())
 
-  })
+	})
 
 }
-
