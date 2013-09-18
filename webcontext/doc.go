17f6c46d13fc31c2afbd0465e00c4fceb35ff99d
Mat Ryer <mat@stretchr.com>
2013-09-18 14:56:57 -0600

    added another link to tests

--- webcontext/doc.go
+++ webcontext/doc.go
@@ -1,2 +1,5 @@
-// The webcontext package is Gowebs implementation of the Context object.
+// The webcontext package is Goweb's implementation of the Context object.
+//
+// If you plan to write unit tests, see also the http://godoc.org/github.com/stretchr/goweb/webcontext/test
+// package.
 package webcontext
