80585b357a3290a66ac621ea21d3aed1af8438f1
Tyler Bunnell <tylerbunnell@gmail.com>
2014-05-14 09:46:11 -0600

    Add test proving X-HTTP-Method-Override

--- handlers/http_handler_test.go
+++ handlers/http_handler_test.go
@@ -2,14 +2,15 @@ package handlers
 
 import (
 	"errors"
+	"net/http"
+	"testing"
+
 	codecsservices "github.com/stretchr/codecs/services"
 	"github.com/stretchr/goweb/context"
 	handlers_test "github.com/stretchr/goweb/handlers/test"
 	"github.com/stretchr/testify/assert"
 	http_test "github.com/stretchr/testify/http"
 	"github.com/stretchr/testify/mock"
-	"net/http"
-	"testing"
 )
 
 func TestNewHttpHandler(t *testing.T) {
@@ -155,6 +156,50 @@ func TestServeHTTP(t *testing.T) {
 
 	responseWriter := new(http_test.TestResponseWriter)
 	testRequest, _ := http.NewRequest("GET", "http://stretchr.org/goweb", nil)
+	codecService := codecsservices.NewWebCodecService()
+	handler := NewHttpHandler(codecService)
+
+	// setup some test handlers
+	handler1 := new(handlers_test.TestHandler)
+	handler2 := new(handlers_test.TestHandler)
+	handler3 := new(handlers_test.TestHandler)
+
+	handler.Handlers = append(handler.Handlers, handler1)
+	handler.Handlers = append(handler.Handlers, handler2)
+	handler.Handlers = append(handler.Handlers, handler3)
+
+	handler1.On("WillHandle", mock.Anything).Return(true, nil)
+	handler1.On("Handle", mock.Anything).Return(false, nil)
+	handler2.On("WillHandle", mock.Anything).Return(true, nil)
+	handler2.On("Handle", mock.Anything).Return(false, nil)
+	handler3.On("WillHandle", mock.Anything).Return(true, nil)
+	handler3.On("Handle", mock.Anything).Return(false, nil)
+
+	handler.ServeHTTP(responseWriter, testRequest)
+
+	mock.AssertExpectationsForObjects(t, handler1.Mock, handler2.Mock, handler3.Mock)
+
+	// get the first context
+	ctx := handler1.Calls[0].Arguments[0].(context.Context)
+
+	assert.Equal(t, responseWriter, ctx.HttpResponseWriter())
+	assert.Equal(t, testRequest, ctx.HttpRequest())
+
+	// make sure it's always the same context
+	ctx1 := handler1.Calls[0].Arguments[0].(context.Context)
+	ctx2 := handler2.Calls[0].Arguments[0].(context.Context)
+	ctx3 := handler3.Calls[0].Arguments[0].(context.Context)
+
+	assert.Equal(t, ctx1, ctx2, "Contexts should be the same")
+	assert.Equal(t, ctx2, ctx3, "Contexts should be the same")
+
+}
+
+func TestServeHTTPMethodOverride(t *testing.T) {
+
+	responseWriter := new(http_test.TestResponseWriter)
+	testRequest, _ := http.NewRequest("POST", "http://stretchr.org/goweb", nil)
+	testRequest.Header.Set("X-HTTP-Method-Override", "GET")
 	codecService := codecsservices.NewWebCodecService()
 	handler := NewHttpHandler(codecService)
 
